8bb1c4cc012093209bfb58463912368a2dda562e
MoonTeamDev <makan.j130130@gmail.com>
2016-12-27 12:43:02 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -110,10 +110,7 @@ function tdcli_update_callback(data)
 			tdcli.exportChatInviteLink(msg.chat_id_)
 			tdcli.sendText(msg.chat_id_, 17, 0, 1, nil, msg.invite_link_, 1, 'md')
 			--tdcli.sendText(msg.chat_id_, 17, 0, 1, nil, 'lonk :'..ChatInviteLink, 1, 'md')
-			elseif input:match('^/pin') then
-        local text = input:gsub('/pin', '')
-			tdcli.pinChannelMessage(msg.chat_id_, text)	
-		elseif input:match('^/typing on$') then
+                        elseif input:match('^/typing on$') then
 			hash = 'typing:'..msg.chat_id_
 			mame:set(hash,true)
 			tdcli.sendText(msg.chat_id_, 17, 0, 1, nil, '_Typing Mode For This Group Has Been Activted_', 1, 'md')
