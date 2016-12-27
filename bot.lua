c6c17680d17d3a67b3f526dd28a5496c3b1b3ecb
MoonTeamDev <makan.j130130@gmail.com>
2016-12-27 15:10:45 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -91,7 +91,7 @@ function tdcli_update_callback(data)
     -- If the message is text message
     if msg.content_.ID == "MessageText" then
       -- And content of the text is...
-      if msg.content_.text == "pin" and msg.content_.reply_to_message_id_ ~= 0 then
+      if input:match('/pin') and msg.content_.reply_to_message_id_ ~= 0 then
 tdcli.pinChannelMessage(msg.content_.chat_id_, msg.content_.reply_to_message_id_, 1)
 tdcli.sendMessage(msg.chat_id_, 0, 1, '<b>پیام پین شد</b>', 1, 'html')
 
