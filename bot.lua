9435ebc34333c7b576cd87bff4650d78d4b8ad4c
MoonTeamDev <makan.j130130@gmail.com>
2016-12-27 18:27:01 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -33,6 +33,18 @@ sudo_users = {
   0
 }
 
+function is_mod(msg)
+local var = false
+ -- — Check users id in config
+for v,user in redis:hget('mod'..msg.chat_id_,msg.sender_user_id_) do
+
+ if user == msg.sender_user_id_ then
+ var = true
+ end
+ end
+ return var
+end
+
 -- Print message format. Use serpent for prettier result.
 function vardump(value, depth, key)
   local linePrefix = ''
