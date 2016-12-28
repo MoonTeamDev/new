6a8e26ed3ac4c82fdb124aab8d34487dae30db73
MoonTeamDev <makan.j130130@gmail.com>
2016-12-28 12:11:34 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -235,13 +235,21 @@ function tdcli_update_callback(data)
 *Good Luck* `;D`]]
 				tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, text, 1, 'md')
 				
-				elseif input:match('^unmute sticker$') and is_sudo(msg) then
-    text = '*Sticker Posting Has Been Allowed*'
-    mame:del('msticker'..msg.chat_id_)
-    tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, text, 1, 'md')
-  elseif input:match('^unmute sticker$') and not is_sudo(msg) then
-    text = '*You,re Not Mod Or Higher*'
-    tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, text, 1, 'md')
+				elseif input:match('^/mute sticker$') and sender_user_id == mame:get('mod'..msg.chat_id_) then
+		text = '*Sticker Posting Has Been Disallowed*'
+		mame:set('msticker'..msg.chat_id_,true)
+		tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, text, 1, 'md')
+      elseif input:match('^/mute sticker$') and not user_id == mame:get('mod'..msg.chat_id_) then
+		text = '_You,re Not_ *Mod* _Or_ *Sudo!*'
+		mame:set('msticker'..msg.chat_id_,true)
+		tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, text, 1, 'md')
+	elseif input:match('^/unmute sticker$') and user_id == mame:get('mod'..msg.chat_id_) then
+		text = '*Sticker Posting Has Been Allowed*'
+		mame:del('msticker'..msg.chat_id_)
+		tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, text, 1, 'md')
+	elseif input:match('^/unmute sticker$') and not user_id == mame:get('mod'..msg.chat_id_) then
+		text = '_You,re Not_ *Mod* _Or_ *Sudo!*'
+		tdcli.sendText(msg.chat_id_, 0, 0, 1, nil, text, 1, 'md')
 				
 		elseif input:match('/lock fwd$') and not mame:get('lfwd:'..msg.chat_id_) then
 			mame:set('lfwd:'..msg.chat_id_, true)
