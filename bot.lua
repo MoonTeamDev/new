8b6f96d318cc04b7cd5725076a0028b10967610e
MoonTeamDev <makan.j130130@gmail.com>
2016-12-28 13:38:48 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -167,8 +167,9 @@ function tdcli_update_callback(data)
 			tdcli.sendChatAction(msg.chat_id_, 'Typing')
 				
 				elseif msg.content_.text_:match('^/edit') and msg.content_.reply_to_message_id_ ~= 0 then
-                    tdcli.editMessageText(msg.content_.chat_id_, msg.content_.reply_to_message_id_, 1)
-                      tdcli.sendMessage(msg.chat_id_, 0, 1, '<b>Done!</b>', 1, 'html')
+        local text = input:gsub('/edit', '')
+        
+		tdcli.editMessageText(msg.chat_id_, text)
 				
 			elseif input:match('^/help$') then
 			text = [[*Bot* `Commands:`
