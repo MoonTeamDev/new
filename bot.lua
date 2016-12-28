87745c8756872e4133771b593b5131fcc75dbe1a
MoonTeamDev <makan.j130130@gmail.com>
2016-12-28 10:35:40 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -36,9 +36,9 @@ sudo_users = {
 function is_mod(msg)
 local var = false
 
-for k,v in pairs(redis:mods('mods'..msg.chat_id_,msg.sender_user_id_)) do
+for k,v in pairs(redis:sismember('mods'..msg.chat_id_,msg.sender_user_id_)) do
 
-if user == mods then
+if user_id == mods then
 var = true
 end
 end
